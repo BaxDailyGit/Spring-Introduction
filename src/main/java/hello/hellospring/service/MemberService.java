11eeed1ded12bbe50eb3d933a827ae453e881d29
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-03-07 23:02:40 +0900

    서비스 클래스 validateDuplicateMember 메서드까지

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,31 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * 회원 가입
+    *  */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원은 안된다.
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+}
